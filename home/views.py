97367a8eb07592dafc467d5e1cae8c9f04d81a47
Reisdrj <davireisjesus@gmail.com>
2025-09-23 09:43:49 -0300

    🎉Project Upload

---
+++ home/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .models import Client
+
+# Create your views here.
+from .forms import *
+
+def redirect_login(request):
+    return redirect('Login')
+
+# def user_login(request):
+#     if request.method == 'GET':
+#         form = LoginForm()
+#         return render(request, 'login.html', {'form': form})
+#     else:
+#         form = LoginForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('dashboard')
+#         return HttpResponse("ERRO")
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
+    
+
+def dashboard(request):
+    return HttpResponse("fera DEMAIS!")
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            client = Client(client=user)
+            return redirect('Login')
+        else:
+            messages.error(request, form.errors)
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
